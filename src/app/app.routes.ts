e283a5310282703f4ac3d5e3cd31f9b94941c8c6
mdsajalali <mdsajalali2020@gmail.com>
2025-06-21 23:10:55 +0600

    added: all products ui

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -29,7 +29,7 @@ export const routes: Routes = [
     component: ContactComponent,
   },
   {
-    path: 'dashboard/all-products',
+    path: 'dashboard/products',
     component: AllProductsComponent,
   },
   {
