b4edd155ea2ca3e26c785fe58f35052ed5a23dc5
mdsajalali <mdsajalali2020@gmail.com>
2025-06-21 20:19:53 +0600

    update: ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,20 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { CategoriesComponent } from './components/categories/categories.component';
+import { FeaturedProductsComponent } from './components/featured-products/featured-products.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    FooterComponent,
+    HeroComponent,
+    CategoriesComponent,
+    FeaturedProductsComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
