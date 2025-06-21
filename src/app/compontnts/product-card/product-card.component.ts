8aea664e9f384e012f9bb3e9adb62e3b13f74394
mdsajalali <mdsajalali2020@gmail.com>
2025-06-21 21:36:04 +0600

    added: product details ui

--- src/app/compontnts/product-card/product-card.component.ts
+++ src/app/compontnts/product-card/product-card.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-product-card',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './product-card.component.html',
-  styleUrl: './product-card.component.css'
+  styleUrl: './product-card.component.css',
 })
-export class ProductCardComponent {
-
-}
+export class ProductCardComponent {}
