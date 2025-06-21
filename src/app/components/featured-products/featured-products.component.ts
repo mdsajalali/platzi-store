04f0e5de88197ed6daf7fc432b280e1cac55d333
mdsajalali <mdsajalali2020@gmail.com>
2025-06-22 00:12:00 +0600

    added: featured product api

--- src/app/components/featured-products/featured-products.component.ts
+++ src/app/components/featured-products/featured-products.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
-import { ProductCardComponent } from "../../compontnts/product-card/product-card.component";
+import { ProductCardComponent } from '../../compontnts/product-card/product-card.component';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../types';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-featured-products',
-  imports: [ProductCardComponent],
+  imports: [ProductCardComponent, CommonModule],
   templateUrl: './featured-products.component.html',
-  styleUrl: './featured-products.component.css'
+  styleUrl: './featured-products.component.css',
 })
 export class FeaturedProductsComponent {
+  products: Product[] = [];
 
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.productService.getProducts().subscribe({
+      next: (res) => {
+        const total = res.length;
+        const maxStart = Math.max(0, total - 4);
+        const randomStart = Math.floor(Math.random() * (maxStart + 1));
+        this.products = res.slice(randomStart, randomStart + 4);
+      },
+      error: (err) => {
+        console.error('Failed to load products', err);
+      },
+    });
+  }
 }
