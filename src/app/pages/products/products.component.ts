0854b85c1e78989d8abe6aff4b355cf13027a56c
mdsajalali <mdsajalali2020@gmail.com>
2025-06-22 00:01:25 +0600

    added: product filter

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,32 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, effect, signal } from '@angular/core';
 import { ProductCardComponent } from '../../compontnts/product-card/product-card.component';
 import { Product } from '../../types';
 import { ProductService } from '../../services/product.service';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-products',
-  imports: [ProductCardComponent, CommonModule],
+  imports: [ProductCardComponent, CommonModule, FormsModule],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css',
 })
 export class ProductsComponent {
-  products: Product[] = [];
-  loading = false;
+  products = signal<Product[]>([]);
+  loading = signal<boolean>(false);
+  searchTitle = signal<string>('');
+  selectedCategory = signal<number | null>(null);
 
-  constructor(private productService: ProductService) {}
-
-  ngOnInit() {
-    this.loading = true;
-    this.productService.getProducts().subscribe({
-      next: (res) => {
-        console.log('Products from API:', res);
-        this.products = res;
-        this.loading = false;
-      },
-      error: () => {
-        this.loading = false;
-      },
+  constructor(private productService: ProductService) {
+    effect(() => {
+      this.fetchProducts();
     });
+  }
+
+  fetchProducts() {
+    this.loading.set(true);
+
+    this.productService
+      .getProducts({
+        title: this.searchTitle(),
+        categoryId: this.selectedCategory() || undefined,
+      })
+      .subscribe({
+        next: (res) => {
+          this.products.set(res);
+          this.loading.set(false);
+        },
+        error: () => this.loading.set(false),
+      });
+  }
+
+  updateSearch(value: string) {
+    this.searchTitle.set(value);
+    this.fetchProducts();
+  }
+
+  filterByCategory(id: number | null) {
+    this.selectedCategory.set(id);
+    this.fetchProducts();
   }
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Product } from '../types';
@@ -11,7 +11,17 @@ export class ProductService {
 
   constructor(private http: HttpClient) {}
 
-  getProducts(): Observable<Product[]> {
-    return this.http.get<Product[]>(this.apiUrl);
+  getProducts(filters?: { title?: string; categoryId?: number }) {
+    let params = new HttpParams();
+
+    if (filters?.title) {
+      params = params.set('title', filters.title);
+    }
+
+    if (filters?.categoryId) {
+      params = params.set('categoryId', filters.categoryId.toString());
+    }
+
+    return this.http.get<Product[]>(this.apiUrl, { params });
   }
 }
