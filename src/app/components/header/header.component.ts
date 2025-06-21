cc76847ddfd0e6981b10ff8efcb605f9bfb83dd1
mdsajalali <mdsajalali2020@gmail.com>
2025-06-21 21:01:52 +0600

    added: header responsive

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -8,4 +8,10 @@ import { RouterLink } from '@angular/router';
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  isSidebarOpen = false;
+
+  toggleSidebar() {
+    this.isSidebarOpen = !this.isSidebarOpen;
+  }
+}
