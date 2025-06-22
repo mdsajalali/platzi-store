cbb2e231e6648bd665ca1b0043f72074b4993081
mdsajalali <mdsajalali2020@gmail.com>
2025-06-22 10:50:29 +0600

    added: product details cart functionality

--- src/app/pages/product-details/product-details.component.ts
+++ src/app/pages/product-details/product-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Product } from '../../types';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { ProductService } from '../../services/product.service';
@@ -44,5 +44,16 @@ export class ProductDetailsComponent implements OnInit {
   changeMainImage(url: string) {
     this.mainImage = url;
   }
-  
+
+  addToCart() {
+    if (!this.product) return;
+    const cart = JSON.parse(localStorage.getItem('cart') || '{}');
+    if (cart[this.product.id]) {
+      cart[this.product.id] += 1;
+    } else {
+      cart[this.product.id] = 1;
+    }
+    localStorage.setItem('cart', JSON.stringify(cart));
+    window.dispatchEvent(new Event('cartUpdated'));
+  }
 }
